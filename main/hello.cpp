bb2c46515557983396b4308b6e21ff3dc0c1ed77
Manas Chebrolu <75514487+manasch@users.noreply.github.com>
2023-02-16 14:47:58 +0530

    Modify hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Changes have been made" << endl;
   return 0;
 }
